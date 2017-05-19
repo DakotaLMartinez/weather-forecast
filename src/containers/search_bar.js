0efaced403f01a62d54df58676ef56227d8595aa
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-05-19 10:41:22 -0700

    Make search field a controlled field

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import { connect }          from 'react-redux';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
   
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group" >
         <input 
-          type="text" />
+          type="text" 
+          placeholder="Get a five-day forecast in your favorite cities" 
+          className="form-control" 
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
