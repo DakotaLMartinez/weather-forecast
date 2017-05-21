35120b7b43dd5019bf3d77e30627d4bc5effb9a7
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-05-20 23:01:14 -0700

    Add reducer_weather to handle updating weather state

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,11 @@
+import { FETCH_WEATHER } from '../actions/index';
+// State argument is not application state, only the state 
+// this reducer is responsible for
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ];
+    default: 
+      return state;
+  }
+}
