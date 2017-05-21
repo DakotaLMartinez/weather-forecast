3442469f15951d51f5be8d39ad7a0c24ab6f00f8
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-05-20 23:27:53 -0700

    Add sparkline chart for temperatures

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
-import { connect }          from 'react-redux';
+import React, { Component }           from 'react';
+import { connect }                    from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderRow(cityData) {
-    const name = cityData.city.name;
+    const name       = cityData.city.name;
+    let temperatures = [];
+    let humidities   = [];
+    let pressures    = [];
+    cityData.list.forEach((weather) => {
+      temperatures.push(weather.main.temp);
+      humidities.push(weather.main.humidity);
+      pressures.push(weather.main.pressure);
+    })
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temperatures}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
