d0db45088dfb2efb30f0f7ad363b72cc3af6dfb5
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-05-20 23:17:15 -0700

    add WeatherList container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect }          from 'react-redux';
+
+class WeatherList extends Component {
+  renderRow(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+  
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>        
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderRow)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  // Whatever is returned will show up as props
+  // inside of WeatherList 
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
